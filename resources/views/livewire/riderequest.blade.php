39e64c729e0e75cd5ebe5797267faca3aadb6c6e
kstar0102 <kstar0102@outlook.com>
2024-06-15 03:56:56 +0900

    .

--- resources/views/livewire/riderequest.blade.php
+++ resources/views/livewire/riderequest.blade.php
@@ -21,14 +21,14 @@
             <h2 class="h4">Ride Request List</h2>
         </div>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <a href="/riderequest/create" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
+            {{-- <a href="/riderequest/create" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
                 <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
                     </path>
                 </svg>
                 New Booking
-            </a>
+            </a> --}}
         </div>
     </div>
     <div class="card card-body shadow border-0 table-wrapper table-responsive">
